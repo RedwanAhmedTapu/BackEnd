589ec01f1b5a7c0ec211d1270181d93488b8bc90
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-07-06 15:19:03 +0600

    mongodb 27001 conncted

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-// mongodb+srv://redwantapu1234:redwan@#$@cluster0.y2wf7zz.mongodb.net/ecomapp?retryWrites=true&w=majority
+
 mongoose
   .connect("mongodb://localhost:27017/ecomsiteRegistration", {
     useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("connnection successfull");
