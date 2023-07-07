0bec6194d2e571ebf2f6850ca11231f445ea1f46
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-07-07 11:19:54 +0600

    backend link added

--- server/expressApp.js
+++ server/expressApp.js
@@ -34,7 +34,7 @@ require("../db/connect");
 // );
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
