dabbfc0fe49d2088888dd4211e2ba731c5cce786
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-07-24 10:24:58 +0600

    jwt token updated

--- server/expressApp.js
+++ server/expressApp.js
@@ -431,6 +431,7 @@ app.post("/messages", async (req, res) => {
 
 function verifyToken(req, res, next) {
   const token = req.headers.authorization;
+  console.log(req.headers.authorization);
 
   if (!token) {
     return res.status(401).json({ error: "No token provided" });
@@ -439,7 +440,7 @@ function verifyToken(req, res, next) {
   // Verify the token using the secret key
   jwt.verify(token, process.env.JWT_SECRETKEY, (err, decoded) => {
     if (err) {
-      return res.status(403).json(console.log(err));
+      return res.status(403).json({error:"wrong token"});
     }
 
     // If the token is valid, set the user information in the request object
